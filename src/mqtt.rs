7b47385a41c3277aec87ea55266f695b9f07d73a
Kerry Johnson <kerry.t.johnson@gmail.com>
2023-02-05 22:24:35 +0000

    feat: Use environment for MQTT username and password

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{env, fs};
 
 use dialoguer::{Input, Password};
 use serde::Deserialize;
@@ -28,25 +28,36 @@ pub enum QoS {
 }
 
 impl ServiceConfig {
-    pub fn load_from_file(path: &String, require_username_password: bool) -> ServiceConfig {
+    pub fn load_from_file(path: &String, prompt_allowed: bool) -> ServiceConfig {
         let config = fs::read_to_string(path).unwrap();
         let mut config: ServiceConfig = serde_yaml::from_str(&config).unwrap();
 
-        if require_username_password {
-            config.mqtt_config.user.get_or_insert_with(|| {
-                Input::new()
-                    .with_prompt("MQTT username: ")
-                    .interact_text()
-                    .unwrap()
-            });
+        config.mqtt_config.user.get_or_insert_with(|| {
+            env::var("MQTT_USERNAME").unwrap_or_else(|_| {
+                if prompt_allowed {
+                    Input::new()
+                        .with_prompt("MQTT username: ")
+                        .interact_text()
+                        .unwrap()
+                } else {
+                    panic!("No configuration value for mqtt_config.user and MQTT_USERNAME not present in environment")
+                }
+            })
+        });
 
-            config.mqtt_config.password.get_or_insert_with(|| {
-                Password::new()
-                    .with_prompt("MQTT password: ")
-                    .interact()
-                    .unwrap()
-            });
-        }
+        config.mqtt_config.password.get_or_insert_with(|| {
+            env::var("MQTT_PASSWORD").unwrap_or_else(|_| {
+                if prompt_allowed {
+                    Password::new()
+                        .with_prompt("MQTT password: ")
+                        .interact()
+                        .unwrap()
+                } else {
+                    panic!("No configuration value for mqtt_config.password and MQTT_PASSWORD not present in environment")
+                }
+            })
+        });
+
         config
     }
 }
